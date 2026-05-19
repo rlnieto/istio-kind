df5d9823570fddc1b220a38045edeecb79203afc
narbond0 <raul@nostro.es>
2026-05-20 00:30:09 +0200

    Istio labels added to default namespace

--- create-cluster.sh
+++ create-cluster.sh
@@ -6,3 +6,6 @@ istioctl install --set profile=demo -y
 
 # Comprobamos que se han creado los pods
 kubectl get pods -n istio-system
+
+# Etiqueta el ns para que istio inyecte el sidecar
+kubectl label namespace default istio-injection=enabled
